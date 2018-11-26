a0106b6f6a323acece7b025b747707237a4ed494
edgarmaron <45045714+edgarmaron@users.noreply.github.com>
2018-11-26 19:06:36 +0200

    Add files via upload

--- src/main/java/org/fasttrackkit/App.java
+++ src/main/java/org/fasttrackkit/App.java
@@ -12,24 +12,24 @@ public class App
         Entertainment cleaning = new Entertainment("CleaningBox");
 
         Food Bacon = new Food("Bacon");
-        Food Fish = new Food ("Fish");
+        Bacon.price = 10;
 
+        Food Fish = new Food ("Fish");
+        Fish.price = 15;
+        
         Dog Spike = new Dog("Spike");
         Cat Miles = new Cat("Miles");
 
         Rescuer John = new Rescuer("John");
         John.feed(Bacon,Spike);
         goingout.activity(John,Spike);
+        John.avalibemoney = 300;
+
 
         Rescuer Fred = new Rescuer("Fred");
         Fred.feed(Fish,Miles);
         cleaning.activity(Fred,Miles);
-
-
-
-
-
-
+        
 
 
     }
